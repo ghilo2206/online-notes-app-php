7caa0cb59ab4012a56f93d70609ce909905ec75d
ghilo2206 <40663800+ghilo2206@users.noreply.github.com>
2022-04-24 09:43:01 +0100

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+//database credentials, please create a database by going to your control panel -> MySQL Management -> Create new database, get the new database credentials and use them to replace the values below:
+$username = "root";
+$password = "";
+$dbname = "complet9_notes";
+
+//copy paste your project root directory below:
+//for example, if your domain is student.host20.uk and you placed your notes app project called NotesApp inside your public_html folder, then your root directory would be: student.host20.uk/NotesApp/ 
+//Please don't forget the forward slash "/" at the end.
+
+$projectRoot = "/projects/NotesAppFinalFinal/";
+?>
